865108c8aeebc9a44a497fdef79950846379a08e
audrey-audrey <audreyuu@gmail.com>
2020-11-26 03:22:03 +0000

    Added Parameterized Queries for students and teachers

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,24 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
 // Command line input
 const input = process.argv.slice(2);
 const cohortName = input[0];
 const limit = input[1] || 5;
 const values = [`%${cohortName}%`, limit];
 
-const query = {
-  text: `
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE $1
-  LIMIT $2; 
-  `, 
-  values, 
-  rowMode: 'array'
-}
-
-pool.query(query)
-.then(res => console.log(res.rows))
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+})
 .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// Command line input
-const input = process.argv.slice(2);
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests 
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${input[0] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+// Command line input
+const input = process.argv.slice(2);
+const limit = input[0] || 5;
+const values = [limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`);
