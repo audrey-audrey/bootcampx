eee206eb7f5ca86d6c1e518980dbbd90f31f5656
audrey-audrey <audreyuu@gmail.com>
2020-11-26 03:18:23 +0000

    Alternative way for Parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,17 +9,22 @@ const pool = new Pool({
 
 // Command line input
 const input = process.argv.slice(2);
+const cohortName = input[0];
+const limit = input[1] || 5;
+const values = [`%${cohortName}%`, limit];
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${input[0]}%' 
-LIMIT ${input[1] || 5}; 
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-})
+const query = {
+  text: `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2; 
+  `, 
+  values, 
+  rowMode: 'array'
+}
+
+pool.query(query)
+.then(res => console.log(res.rows))
 .catch(err => console.error('query error', err.stack));
